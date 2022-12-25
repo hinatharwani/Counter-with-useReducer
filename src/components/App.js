bac00ce6add8bbac2682d6fb2eb111643bc2bb92
HinaTharwani <113449700+hinatharwani@users.noreply.github.com>
2022-12-26 01:43:16 +0530

    upload

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
-import React, { useReducer } from 'react'
+import React, { useReducer, useState } from 'react'
 import { counterReducer } from '../reducers/counterReducer';
 import '../styles/App.css';
 const App = () => {
-const [state,dispatch] = useReducer(counterReducer,{/*initial state to be placed here*/})
+const initialState = {count:0};
+const [state,dispatch] = useReducer(counterReducer,initialState)
+const [fName,setName] = useState('sumit')
+
+const handleUpdateName = () => {
+  setName('Raghu')
+}
   return (
     <div id="main">
-
+      <p>Count: {state.count} {fName}</p>
+      <button onClick={() => dispatch({type:'increment'})}>INC</button>
+      <button onClick={() => dispatch({type:'decrement'})}>DEC</button>
+      <button onClick={handleUpdateName}>update Fname</button>
     </div>
   )
 }
